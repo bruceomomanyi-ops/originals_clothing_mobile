a5e5f5304175d0d7e62e87905c796e5be2037dbb
bruceomomanyi-ops <itsbruceomomanyi@gmail.com>
2026-02-04 13:39:31 +0300

    Initial commit: Originals Clothing Flutter app with fixed images and button

---
+++ android/app/src/main/kotlin/com/example/originals_clothing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.originals_clothing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebViewPage(),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  @override
+  _WebViewPageState createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = WebViewController();
+    controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+    controller.loadFlutterAsset('assets/html/index.html');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(
+          controller: controller,
+        ),
+      ),
+    );
+  }
+}
